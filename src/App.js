5812a885869b04f363546aa5382ac882efce3123
Stasys Meclazcke <aeip@live.com>
2020-10-21 00:27:51 -0500

    App almost ready and form is ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
+import Display from './Display';
+import Form from './Form';
 
 function App() {
+  const url = 'https://mernbackendsvm.herokuapp.com';
+  const [genres, setGenres];
+  const [movies, setMovies];
+
+  const emptyMovie = {
+    title: '',
+    year: 0,
+    img: '',
+    genre: ''
+  }
+
+const [selectedMovie, setSelectedMovie] = React.useState(emptyMovie);
+const [selectedGenre, setSelectedGenre] = React.useState({});
+
+const getMovies = () => {
+  fetch(url + '/movie/')
+  .then((response) => response.json())
+  .then((data) => {
+    setMovies(data);
+  });
+};
+
+React.useEffect(() => {
+  getMovies();
+},[]);
+
+const handleCreate = (newMovie) => {
+  fetch(url + '/movie/', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newMovie)
+  }).then((response) => getMovies());
+};
+
+const handleUpdate = (movie) => {
+  fetch(url + '/movie/' + movie._id, {
+    method: 'put',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(movie)
+  })
+  .then(() => { getMovies()})
+}
+
+const selectMovie = (movie) => {
+  setSelectedMovie(movie);
+};
+
+const deleteMovie = (movie) => {
+  fetch(url + '/movie/' + movie._id, {
+    method: 'delete'
+  })
+  .then(() => { getMovies()})
+}
+
+const selectGenre = (genre) => {
+  setSelectedGenre(genre);
+}
+
+const handleGenre = (genre) => {
+  const getMovies = (movie) => {
+    
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to='/create'>
+				<button>Add Dog</button>
+			</Link>
+      <switch>
+        <Route exactpath="/" render={(rp) => <Genres {...rp} genres={genres} selectGenre={selectGenre} />}
+        />
+        <Route exactpath="/movie/" render={(rp) => <Movies {...rp} movies={movies} selectMovie={selectMovie} deleteMovie={deleteMovie} />}
+        />
+        <Route
+						exact
+						path='/create'
+						render={(rp) => (
+							<Form
+								{...rp}
+								label='create'
+								movie={emptyMovie}
+								handleSubmit={handleCreate}
+							/>
+						)}
+					/>
+					<Route
+						exact
+						path='/edit'
+						render={(rp) => (
+							<Form {...rp} label='update' movie={selectedMovie} handleSubmit={handleUpdate} />
+						)}
+					/>
+      </switch>
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,44 @@
+import React from 'react';
+const Form = (props) => {
+	//STATE FOR THE FORM
+	const [formData, setFormData] = React.useState(props.movie);
+	//FUNCTIONS
+	const handleSubmit = (event) => {
+		event.preventDefault(); // Prevent Form from Refreshing
+		props.handleSubmit(formData); // Submit to Parents desired function
+		props.history.push('/'); //Push back to display page
+	};
+	const handleChange = (event) => {
+		setFormData({ ...formData, [event.target.name]: event.target.value });
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type='text'
+				name='title'
+				value={formData.name}
+				onChange={handleChange}
+			/>
+			<input
+				type='number'
+				name='year'
+				value={formData.age}
+				onChange={handleChange}
+			/>
+			<input
+				type='text'
+				name='img'
+				value={formData.img}
+				onChange={handleChange}
+			/>
+			<input
+				type='text'
+				name='genre'
+				value={formData.genre}
+				onChange={handleChange}
+			/>
+			<input type='submit' value={props.label} />
+		</form>
+	);
+};
+export default Form;
